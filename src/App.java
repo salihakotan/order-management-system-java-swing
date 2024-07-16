eedaceba7a320b0ee8af888b31f9487d14ef57bb
Saliha Kotan <salihakotan77@gmail.com>
2024-07-16 11:17:13 +0300

    basket, cart, product ui controllers, entities. and project completed

--- src/App.java
+++ src/App.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class App {
     public static void main(String[] args) {
         Helper.setTheme();
